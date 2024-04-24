cd20a0f6371927018c82a8020b938a49515dc974
zeepsopje <stefanhuynh98@gmail.com>
2024-04-24 20:46:20 +0200

    very minor code cleanups

--- client/src/lib/ui/Stepper.svelte
+++ client/src/lib/ui/Stepper.svelte
@@ -25,26 +25,17 @@
 </script>
 
 <div class="flex h-[40px] items-stretch gap-[1px] relative">
-	<button
-		class="group flex w-[40px] items-center justify-center bg-gray hover:bg-primary"
-		on:click={decrement}
-	>
+	<button class="group flex w-[40px] items-center justify-center bg-gray hover:bg-primary" on:click={decrement}>
 		<img
 			src="/arrow-down.svg"
 			class="rotate-90 group-hover:invert"
 			alt="decrement"
 		/>
 	</button>
-	<button
-		class="w-[200px] h-full px-4 bg-gray text-xs"
-		on:click={reset}
-	>
+	<button class="w-[200px] h-full px-4 bg-gray text-xs" on:click={reset}>
 		{label}
 	</button>
-	<button
-		class="group flex w-[40px] items-center justify-center bg-gray hover:bg-primary"
-		on:click={increment}
-	>
+	<button class="group flex w-[40px] items-center justify-center bg-gray hover:bg-primary" on:click={increment}>
 		<img
 			src="/arrow-down.svg"
 			class="-rotate-90 group-hover:invert"
@@ -52,16 +43,10 @@
 		/>
 	</button>
 	{#if returnLabel && counter !== 0}
-		<div
-			class="absolute flex justify-center w-full top-full mt-[3px] left"
-			
-		>
-			<button
-				class="text-xs hover:underline opacity-80"
-				on:click={reset}
-			>
+		<div class="absolute flex justify-center w-full top-full mt-[3px] left">
+			<button class="text-xs hover:underline opacity-80" on:click={reset}>
 				{returnLabel}
 			</button>
-	</div>
+		</div>
 	{/if}
 </div>
